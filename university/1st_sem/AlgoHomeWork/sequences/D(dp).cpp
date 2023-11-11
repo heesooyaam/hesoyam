c0fcbb7288dfa637953c76d9e402999c93dd471b
heesooyaam <mr.filonets-anton@yandex.ru>
2023-11-11 11:08:29 +0300

    sjdksdj

---
+++ university/1st_sem/AlgoHomeWork/sequences/D(dp).cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define eb emplace_back
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define ss second
+#define ff first
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define print(x); for(auto& val : x){cout << val << ' ';}cout << endl;
+#define input(x); for(auto& val : x){cin >> val;}
+#define make_unique(x) sort(all((x))); (x).resize(unique(all((x))) - (x).begin())
+#define endl '\n'   
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<vector<int>> p(k, vector<int> (n));
+    vector<vector<int>> pos(k, vector<int> (n + 1));
+    for(int i = 0; i < k; ++i)
+    {
+        for(int j = 0; j < n; ++j)
+        {
+            cin >> p[i][j];
+            pos[i][p[i][j]] = j;
+        }
+    }
+
+    vector<int> dp(n, 1);
+    //dp[i] = длина наибольшей подпоследовательности, которая закончилась в i
+    for(int i = 1; i < n; ++i)
+    {
+        for(int j = 0; j < i; ++j)
+        {
+            bool ok = true;
+            for(int q = 1; q < k && ok; ++q)
+            {
+                ok = pos[q][p[0][j]] < pos[q][p[0][i]]; 
+            }
+            if(ok) dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+    cout << *max_element(all(dp)) << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    int t = 1; // cin >> t;
+    while(t--) solve();
+    return 0;
+}
---
+++ university/1st_sem/lessons/november/111123/shared_ptr.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+
+template <typename T>
+struct SharedData
+{
+    T* ptr_;
+    size_t sharedCounter;
+    size_t weakCounter;
+};
+
+template<typename T>
+class SharedPtr
+{
+    friend WeekPointer;
+private:
+    SharedData<T>* data;
+    SharedPtr(SharedData<T>* otherData) 
+        : data_(otherData) {}
+public:
+    SharedPtr()
+        : data(new SharedData<T>(ptr_ = new T(), sharedCounter = 1, weakCounter = 0)) {
+    } 
+
+    SharedPtr(const SharedPtr& other) 
+        : data(other.data) {}
+
+    ~SharedPtr() {
+        data->sharedCounter -= 1;
+        if (data->sharedCounter == 0 && data->ptr != nullptr) {
+            delete data->ptr_;
+            data->ptr_ = nullptr;
+        }
+        if(data->sharedCounter + data->weakCounter == 0)
+        {
+            delete data;
+        }
+    }
+    T& operator*() {
+        return *ptr_;
+    }
+    const T& operator*() const {
+        return *ptr_;
+    }
+
+    T* operator->() {
+        return ptr_;
+    }
+    const T* operator->() const {
+        return ptr_;
+    }
+
+    T* Get() {
+        return ptr_;
+    }
+    const T* Get() const {
+        return ptr_;
+    }
+};
+
+template <typename T>
+class WeakPointer
+{
+private:
+    SharedData<T>* data;
+public:
+    WeakPointer(SharedPointer<T>& shdPtr)
+    : data(shdPtr.data)
+    {
+        ++data->weakCounter;
+    }
+    ~WeakPointer()
+    {
+        --(data->weakCounter);
+        if(data->sharedCounter + data->weakCounter == 0)
+        {
+            delete data;
+        }
+    }
+    SharedPtr<T> lock() const;
+    {
+        return SharedPtr(data);
+    }
+};
+
+SharedPtr<int> Foo(int x) {
+    SharedPtr<int> result;
+    *result = x;
+    return result;
+}
+
+int main() {
+    /* FIRST
+
+        SharedPtr<int> p;
+        std::cout << *p << " " << p.Get() << std::endl;
+        *p = 5;
+        std::cout << *p << " " << p.Get() << std::endl;
+    
+    */
+
+        std::vector<SharedPtr<int>> ptrs;
+        for (int i = 0; i < 1e8; ++i) {
+            auto p = Foo(i);
+            std::cout << *p << "\n";
+            ptrs.push_back(p);
+        }
+   /* Second
+
+    */
+    return 0;
+}
